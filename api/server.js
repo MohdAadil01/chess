b722866ae270ab9b24448bd3ff29d851ef2d86ac
MohdAadil01 <mohdaadil4240@gmail.com>
2024-05-29 20:24:16 +0530

    setup backend

--- api/server.js
+++ api/server.js
@@ -1,3 +1,4 @@
+const { Chess } = require("chess.js");
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
@@ -10,18 +11,46 @@ const io = socketIo(server, {
     methods: ["GET", "POST"],
   },
 });
+const chess = new Chess();
+let players = {};
+let currentPlayer = "w";
 
 io.on("connection", (socket) => {
-  console.log("New client connected");
+  if (!players.white) {
+    players.white = socket.id;
+    socket.emit("playerRole", "w");
+  } else if (!players.black) {
+    players.black = socket.id;
+    socket.emit("playerRole", "b");
+  } else {
+    socket.emit("spectatorRole");
+  }
 
-  // Handle events from the client
-  socket.on("message", (message) => {
-    console.log(`Message from client: ${message}`);
+  socket.on("move", (move) => {
+    try {
+      if (chess.turn() === "w" && socket.id !== players.white) return;
+      if (chess.turn() === "b" && socket.id !== players.black) return;
+
+      const result = chess.move(move);
+      if (result) {
+        currentPlayer = chess.turn();
+        io.emit("move", move);
+        io.emit("boardState", chess.fen());
+      } else {
+        console.log("Invalid move :" + move);
+        socket.emit("invalidMove", move);
+      }
+    } catch (error) {
+      console.log("Error while handling move " + error);
+    }
   });
 
-  // Handle client disconnect
   socket.on("disconnect", () => {
-    console.log("Client disconnected");
+    if (socket.id === players.white) {
+      delete players.white;
+    } else if (socket.id === players.black) {
+      delete players.black;
+    }
   });
 });
 
--- chess/src/App.jsx
+++ chess/src/App.jsx
@@ -3,57 +3,11 @@ import socketIOClient from "socket.io-client";
 
 const SOCKET_SERVER_URL = "http://localhost:4000"; // Your server URL
 
+const socket = socketIOClient(SOCKET_SERVER_URL);
 const App = () => {
-  const [message, setMessage] = useState("");
-  const [messages, setMessages] = useState([]);
-
-  useEffect(() => {
-    const socket = socketIOClient(SOCKET_SERVER_URL);
-
-    socket.on("connect", () => {
-      console.log("Connected to server");
-    });
-
-    socket.on("disconnect", () => {
-      console.log("Disconnected from server");
-    });
-
-    // Listen for messages from the server
-    socket.on("message", (msg) => {
-      setMessages((prevMessages) => [...prevMessages, msg]);
-    });
-
-    // Clean up the connection when the component is unmounted
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
-
-  const sendMessage = () => {
-    const socket = socketIOClient(SOCKET_SERVER_URL);
-    socket.emit("message", message);
-    setMessage("");
-  };
-
   return (
     <div>
       <h1>Socket.IO React App</h1>
-      <div>
-        <input
-          type="text"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
-        <button onClick={sendMessage}>Send</button>
-      </div>
-      <div>
-        <h2>Messages:</h2>
-        <ul>
-          {messages.map((msg, index) => (
-            <li key={index}>{msg}</li>
-          ))}
-        </ul>
-      </div>
     </div>
   );
 };
